5a387bfc37f574e6488084ca4703b3b42b8a21a2
HafizTerudin <nws04_01050156@pb.edu.bn>
2018-02-13 13:58:15 +0800

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+
 import { NavController } from 'ionic-angular';
+
+import {TestPagePage} from '../test-page/test-page';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +14,11 @@ export class HomePage {
 
   }
 
+  
+}
+export class TestPage{
+  testPagePage=TestPagePage;
+  constructor(){
+
+  }
 }
